af896d0b89a02f3004c161c1e02549d0b690e632
Tomisin Salu <ois5092@psu.edu>
2024-09-02 13:02:43 -0400

    FEAT: 3rd commit

---
+++ Workflow2/Workflow2/Workflow2App.swift
@@ -0,0 +1,17 @@
+//
+//  Workflow2App.swift
+//  Workflow2
+//
+//  Created by Tomisin Salu on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct Workflow2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Workflow2/Workflow2UITests/Workflow2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Workflow2UITestsLaunchTests.swift
+//  Workflow2UITests
+//
+//  Created by Tomisin Salu on 9/2/24.
+//
+
+import XCTest
+
+final class Workflow2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
