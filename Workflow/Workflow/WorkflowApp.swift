0cd8bfff86955745e869a4cd5592a430cabcd00a
Tomisin Salu <ois5092@psu.edu>
2024-09-01 21:49:34 -0400

    first commit

---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Tomisin Salu on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
